133655ef8057c883b15137beab60bcceff327f89
Rodrigo Nas <proclnas@gmail.com>
2019-04-28 03:20:49 -0300

    Added other CRUD routes
    - Update and delete routes
    
    Added json write

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ type Todo struct {
 	Date        string
 }
 
+var todosFile = "todos.json"
 var r = mux.NewRouter()
 var todos []Todo
 
@@ -27,7 +28,7 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 }
 
-// GetTodos return all registred todosin todos.json
+// GetTodos return all registred todos in todosFile
 func GetTodos(w http.ResponseWriter, r *http.Request) {
 	jsonContent, err := openTodoFile()
 
@@ -61,12 +62,45 @@ func GetTodo(w http.ResponseWriter, r *http.Request) {
 
 // CreateTodo return a single todo based on todo id
 func CreateTodo(w http.ResponseWriter, r *http.Request) {
+	jsonContent, err := openTodoFile()
+	params := mux.Vars(r)
+	var todo Todo
 
+	if err != nil {
+		fmt.Fprintf(w, "%s", err)
+	}
+
+	json.Unmarshal(jsonContent, &todos)
+	_ = json.NewDecoder(r.Body).Decode(&todo)
+	todo.ID = params["id"]
+	todos = append(todos, todo)
+
+	saveTodoFile(todos)
+	json.NewEncoder(w).Encode(todos)
 }
 
 // DeleteTodo return a single todo based on todo id
 func DeleteTodo(w http.ResponseWriter, r *http.Request) {
+	jsonContent, err := openTodoFile()
 
+	if err != nil {
+		fmt.Fprintf(w, "%s", err)
+	}
+
+	var newTodos []Todo
+	json.Unmarshal(jsonContent, &todos)
+
+	params := mux.Vars(r)
+	for _, todo := range todos {
+		if todo.ID == params["id"] {
+			continue
+		}
+
+		newTodos = append(newTodos, todo)
+	}
+
+	saveTodoFile(newTodos)
+	json.NewEncoder(w).Encode(newTodos)
 }
 
 func registerRoutes() {
@@ -77,7 +111,7 @@ func registerRoutes() {
 	todosRouter.HandleFunc("/", GetTodos).Methods("GET")
 	todosRouter.HandleFunc("/{id}", GetTodo).Methods("GET")
 	todosRouter.HandleFunc("/{id}", DeleteTodo).Methods("DELETE")
-	todosRouter.HandleFunc("/", CreateTodo).Methods("POST")
+	todosRouter.HandleFunc("/{id}", CreateTodo).Methods("POST")
 
 	// Static content
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static")))
@@ -89,7 +123,7 @@ func registerRoutes() {
 
 // OpenTodoFile return todos.json content
 func openTodoFile() ([]byte, error) {
-	jsonFile, err := os.Open("todos.json")
+	jsonFile, err := os.Open(todosFile)
 
 	if err != nil {
 		return make([]byte, 0), errors.New("Error opening todos file")
@@ -97,6 +131,11 @@ func openTodoFile() ([]byte, error) {
 
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 	return byteValue, nil
+}
+
+func saveTodoFile(newTodos []Todo) {
+	newJSONContent, _ := json.Marshal(newTodos)
+	ioutil.WriteFile(todosFile, newJSONContent, 0644)
 }
 
 func main() {
